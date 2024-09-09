3a6a9861cba876733b946dc7863b58800ed3518a
DevFast Project <devfastproject@gmail.com>
2024-09-08 20:46:03 -0400

    Update database_client.py

--- clients/Python/database_client.py
+++ clients/Python/database_client.py
@@ -1,46 +1,48 @@
 import socketio
 import json
 import asyncio
-from logger_config import logger
+from loguru import logger
 
-class DB:
-    sio = socketio.AsyncClient()
-    connected = False
-    
-    @classmethod
-    async def __call__(cls, operation, **kwargs):
-        if operation == 'connect':
-            if not cls.connected:
-                logger.info(f"Conectando a {kwargs['url']}...")
-                await cls.sio.connect(kwargs['url'])
-                cls.connected = True
-                logger.warning("Conectado al servidor")
-            return
-        
-        if operation == 'disconnect':
-            if cls.connected:
-                logger.info("Desconectando del servidor...")
-                await cls.sio.disconnect()
-                cls.connected = False
-                logger.warning("Desconectado del servidor")
-            return
+sio = socketio.AsyncClient()
+connected = False
 
-        if not cls.connected:
-            logger.error("No conectado al servidor")
-            raise Exception("Not connected to server")
 
-        params = json.dumps(kwargs)
-        future = asyncio.get_event_loop().create_future()
+async def connect(url):
+    global connected
+    if not connected:
+        logger.info(f"Conectando a {url}...")
+        await sio.connect(url)
+        connected = True
+        logger.warning("Conectado al servidor")
 
-        @cls.sio.on('response')
-        def on_response(data):
-            logger.debug(f"Respuesta recibida: {data}")
-            future.set_result(json.loads(data))
-            cls.sio.off('response')
 
-        logger.info(f"Enviando operación {operation} con parámetros: {params}")
-        await cls.sio.emit(operation, params)
-        return await future
+async def disconnect():
+    global connected
+    if connected:
+        logger.info("Desconectando del servidor...")
+        await sio.disconnect()
+        connected = False
+        logger.warning("Desconectado del servidor")
+
+
+async def execute(operation, **kwargs):
+    global connected
+    if not connected:
+        logger.error("No conectado al servidor")
+        raise Exception("Not connected to server")
+
+    params = json.dumps(kwargs)
+    future = asyncio.get_event_loop().create_future()
+
+    @sio.on("response")
+    def on_response(data):
+        logger.debug(f"Respuesta recibida: {data}")
+        future.set_result(json.loads(data))
+
+    logger.info(f"Enviando operación {operation} con parámetros: {params}")
+    await sio.emit(operation, params)
+    return await future
+
 
 Types = {
     "NUMBER": "Number",
